41924875d0ded760c6ff9bf8e9f64d4ce36489e0
GyucheolJung <ggyu@GyucheolJungui-iMac.local>
2020-12-01 00:11:22 +0900

    복습

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+
+const { User } = require('./models/User')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+mongoose.connect( config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+    }).then(() => console.log('MongoDB Connected....'))
+    .catch(err => console.log(err));
+
+
+app.get('/', (req,res) => res.send('Hello World!!'))
+
+app.post('/register', (req,res) => {
+    // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
